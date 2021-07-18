2ae986b22648506608e91d73b55289ffdf273980
Glen Maytum <glenmaytum@gmail.com>
2021-07-18 18:46:09 +0100

    add category filtering

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,6 +17,16 @@ const initialState = {
 	all_products: [],
 	grid_view: true,
 	sort: "price-lowest",
+	filters: {
+		text: "",
+		company: "all",
+		category: "all",
+		color: "all",
+		min_price: 0,
+		max_price: 0,
+		price: 0,
+		shipping: false,
+	},
 };
 
 const FilterContext = React.createContext();
@@ -29,8 +39,9 @@ export const FilterProvider = ({ children }) => {
 	}, [products]);
 
 	useEffect(() => {
+		dispatch({ type: FILTER_PRODUCTS });
 		dispatch({ type: SORT_PRODUCTS });
-	}, [products, state.sort]);
+	}, [products, state.sort, state.filters]);
 
 	const setGridView = () => {
 		dispatch({ type: SET_GRIDVIEW });
@@ -44,6 +55,17 @@ export const FilterProvider = ({ children }) => {
 		dispatch({ type: UPDATE_SORT, payload: value });
 	};
 
+	const updateFilters = (e) => {
+		let name = e.target.name;
+		let value = e.target.value;
+		if (name === "category") {
+			value = e.target.textContent;
+		}
+		dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+	};
+
+	const clearFilters = () => {};
+
 	return (
 		<FilterContext.Provider
 			value={{
@@ -51,6 +73,8 @@ export const FilterProvider = ({ children }) => {
 				setGridView,
 				setListView,
 				updateSort,
+				updateFilters,
+				clearFilters,
 			}}
 		>
 			{children}
