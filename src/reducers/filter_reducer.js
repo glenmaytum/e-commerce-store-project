7f68af8c78173535fb06a0532e3a39e358acffbd
Glen Maytum <glenmaytum@gmail.com>
2021-07-19 12:28:36 +0100

    add text filter functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -60,7 +60,34 @@ const filter_reducer = (state, action) => {
 	}
 
 	if (action.type === FILTER_PRODUCTS) {
-		return { ...state };
+		const { all_products } = state;
+		const { text, category, company, color, price, shipping } =
+			state.filters;
+
+		let tempProducts = [...all_products];
+		//filtering
+		if (text) {
+			tempProducts = tempProducts.filter((product) => {
+				return product.name.toLowerCase().startsWith(text);
+			});
+		}
+
+		return { ...state, filtered_products: tempProducts };
+	}
+
+	if (action.type === CLEAR_FILTERS) {
+		return {
+			...state,
+			filters: {
+				...state.filters,
+				text: "",
+				company: "all",
+				category: "all",
+				color: "all",
+				price: state.filters.max_price,
+				shipping: false,
+			},
+		};
 	}
 
 	throw new Error(`No Matching "${action.type}" - action type`);
